04aa4d3fbd4e4894e5d946424d1af3b7a79efbf7
peter-sila1 <peter.sila@student.moringaschool.com>
2023-08-07 12:10:56 +0300

    test pass

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+console.log(multiply); // output: 62
+
+
+let random = Math.ceil(Math.random() * 100);
+console.log(random);// output: Random number greater than 0
+
+
+let num3 = 14;
+let num4 = 10;
+let mod = num3 % num4;
+console.log(mod); // output: 4
+
+
+let setOfNumbers = [5, 10, 15, 20, 3];
+let max = Math.max(...setOfNumbers);
+comsole.log(max); // output:20
